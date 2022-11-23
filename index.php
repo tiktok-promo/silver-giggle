973af61d9e399c31592af4ef3c69d54498b56855
tiktok-promo <117438241+tiktok-promo@users.noreply.github.com>
2022-11-23 14:06:39 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,22 +1,6 @@
-<?php
-$channel_id = !empty($_GET['channel_id']) ? $_GET['channel_id'] : "-1001512301400";
-$url = !empty($_GET['url']) ? $_GET['url'] : "S_rpcEQLw9c3NTAy";
-
-	// Получение данных канала
-	$actionUrl = "https://api.admindashboard.me/api/v2/channels/$channel_id";
-	$resp = file_get_contents($actionUrl);
-
-	$data = json_decode($resp);
-	$name = $data->model->name;
-	$subscribers = $data->model->subscribers;
-	$logo = $data->model->logo;
-	$description = $data->model->description;
-
-	$desc = str_replace("\n", "<br>", "$description");
-	// Получение данных канала
-?>
+<?php $url = !empty($_GET['url']) ? $_GET['url'] : "S_rpcEQLw9c3NTAy"; ?>
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ru">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -39,21 +23,11 @@ $url = !empty($_GET['url']) ? $_GET['url'] : "S_rpcEQLw9c3NTAy";
 
         <div class="item button-hand" style="--bg-color: #3498db">
             <button>
-                <a href="#"> Подписаться</a>
+                <a href="/subscribe.php/?url=<? echo $url ?>"> Подписаться</a>
               <div class="hands"></div>
             </button>
           </div>
       </div>
   <script src="pril.js"></script>
-  <script type="text/javascript">
-  var searchQuery = window.location.search;
-  var urlParams = new URLSearchParams(window.location.search);
-    var affLink = 'https://t.me/+<? echo $url ?>';
-
-  var anchors = document.getElementsByTagName("a");
-  for (var i = 0; i < anchors.length; i++) {
-    anchors[i].href = affLink;
-  }
-  </script>
 </body>
 </html>
